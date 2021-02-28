d29e59a1b3576003d936b927f56e6d9ceed60001
Wilson Wu <Wilson Wu@DESKTOP-PTUGGNT>
2021-02-28 22:15:24 +0800

    main

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+//"github.com/line/line-bot-sdk-go/linebot"
+
+func main() {
+	fmt.Print("hi")
+	//bot, err := linebot.New("<channel secret>", "<channel access token>")
+	//fmt.Print(bot + err)
+}
