ca4b00fe4cca7399acb0e42bd1ab0875e0b466e9
Wilson Wu <Wilson Wu@DESKTOP-PTUGGNT>
2021-03-01 21:29:29 +0800

    test

--- main.go
+++ main.go
@@ -1,3 +1,61 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"strconv"
+
+// 	"github.com/line/line-bot-sdk-go/linebot"
+// )
+
+// var bot *linebot.Client
+
+// func main() {
+// 	log.Println("hi")
+
+// 	var err error
+// 	botToken := os.Getenv("botToken")
+// 	botSecretKey := os.Getenv("botSecretKey")
+// 	log.Printf(botToken)
+// 	log.Printf(botSecretKey)
+// 	bot, err := linebot.New(botSecretKey, botToken)
+// 	log.Println("Bot:", bot, " err:", err)
+// 	http.HandleFunc("/callback", response)
+
+// 	port := os.Getenv("PORT")
+// 	addr := fmt.Sprintf(":%s", port)
+// 	http.ListenAndServe(addr, nil)
+// }
+
+// func response(writer http.ResponseWriter, request *http.Request) {
+// 	log.Printf("response")
+// 	botEvents, err := bot.ParseRequest(request)
+// 	if err != nil {
+// 		if err == linebot.ErrInvalidSignature {
+// 			writer.WriteHeader(400)
+// 		} else {
+// 			writer.WriteHeader(500)
+// 		}
+// 		return
+// 	}
+
+// 	for _, botEvent := range botEvents {
+// 		if botEvent.Type == linebot.EventTypeMessage {
+// 			switch message := botEvent.Message.(type) {
+// 			case *linebot.TextMessage:
+// 				quota, err := bot.GetMessageQuota().Do()
+// 				if err != nil {
+// 					log.Println("Quota err:", err)
+// 				}
+// 				if _, err = bot.ReplyMessage(botEvent.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+// 					log.Print(err)
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 package main
 
 import (
@@ -13,43 +71,36 @@ import (
 var bot *linebot.Client
 
 func main() {
-	log.Println("hi")
-
 	var err error
-	botToken := os.Getenv("botToken")
-	botSecretKey := os.Getenv("botSecretKey")
-	log.Printf(botToken)
-	log.Printf(botSecretKey)
-	bot, err := linebot.New(botSecretKey, botToken)
+	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
 	log.Println("Bot:", bot, " err:", err)
-	http.HandleFunc("/callback", response)
-
+	http.HandleFunc("/callback", callbackHandler)
 	port := os.Getenv("PORT")
 	addr := fmt.Sprintf(":%s", port)
 	http.ListenAndServe(addr, nil)
 }
 
-func response(writer http.ResponseWriter, request *http.Request) {
-	log.Printf("response")
-	botEvents, err := bot.ParseRequest(request)
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := bot.ParseRequest(r)
+
 	if err != nil {
 		if err == linebot.ErrInvalidSignature {
-			writer.WriteHeader(400)
+			w.WriteHeader(400)
 		} else {
-			writer.WriteHeader(500)
+			w.WriteHeader(500)
 		}
 		return
 	}
 
-	for _, botEvent := range botEvents {
-		if botEvent.Type == linebot.EventTypeMessage {
-			switch message := botEvent.Message.(type) {
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
 				quota, err := bot.GetMessageQuota().Do()
 				if err != nil {
 					log.Println("Quota err:", err)
 				}
-				if _, err = bot.ReplyMessage(botEvent.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
 					log.Print(err)
 				}
 			}
