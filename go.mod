f975ad9da80aee3e69c972fbb3fe49b2d235824c
wilson wu <vinex0153@gmail.com>
2021-03-01 15:37:19 +0800

    copy from example

---
+++ go.mod
@@ -0,0 +1,5 @@
+module myLineBot
+
+go 1.16
+
+require github.com/line/line-bot-sdk-go v7.7.0+incompatible
--- main.go
+++ main.go
@@ -2,12 +2,50 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-//"github.com/line/line-bot-sdk-go/linebot"
+var bot *linebot.Client
 
 func main() {
-	fmt.Print("hi")
-	//bot, err := linebot.New("<channel secret>", "<channel access token>")
-	//fmt.Print(bot + err)
+	fmt.Println("hi")
+
+	botToken := os.Getenv("botToken")
+	botSecretKey := os.Getenv("botSecretKey")
+
+	bot, err := linebot.New(botSecretKey, botToken)
+	http.HandleFunc("/callback", response)
+	fmt.Println(bot)
+	fmt.Println(err)
+}
+
+func response(writer http.ResponseWriter, request *http.Request) {
+	botEvents, err := bot.ParseRequest(request)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			writer.WriteHeader(400)
+		} else {
+			writer.WriteHeader(500)
+		}
+		return
+	}
+
+	for _, botEvent := range botEvents {
+		if botEvent.Type == linebot.EventTypeMessage {
+			switch message := botEvent.Message.(type) {
+			case *linebot.TextMessage:
+				quota, err := bot.GetMessageQuota().Do()
+				if err != nil {
+
+				}
+				if _, err = bot.ReplyMessage(botEvent.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+
+				}
+			}
+		}
+	}
 }
