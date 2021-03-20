9bc184dc6598ec2ca550d92eedcc6a64551e6aab
Wilson Wu <Wilson Wu@DESKTOP-PTUGGNT>
2021-03-20 17:18:01 +0800

    試串API

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 var bot *linebot.Client
 
+var convetedData []map[string]interface{}
+
 func main() {
-	log.Println("hi")
+	getEnvData()
 
 	var err error
 	// botToken := os.Getenv("botToken")
@@ -27,6 +30,7 @@ func main() {
 	port := os.Getenv("PORT")
 	addr := fmt.Sprintf(":%s", port)
 	http.ListenAndServe(addr, nil)
+
 }
 
 func response(writer http.ResponseWriter, request *http.Request) {
@@ -45,14 +49,51 @@ func response(writer http.ResponseWriter, request *http.Request) {
 		if botEvent.Type == linebot.EventTypeMessage {
 			switch message := botEvent.Message.(type) {
 			case *linebot.TextMessage:
-				quota, err := bot.GetMessageQuota().Do()
+				//quota, err := bot.GetMessageQuota().Do()
 				if err != nil {
 					log.Println("Quota err:", err)
 				}
-				if _, err = bot.ReplyMessage(botEvent.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+				if _, err = bot.ReplyMessage(botEvent.ReplyToken, linebot.NewTextMessage(findData(message.Text))).Do(); err != nil {
 					log.Print(err)
 				}
 			}
 		}
 	}
+}
+
+func getEnvData() {
+	resp, err := http.Get("https://data.epa.gov.tw/api/v1/aqx_p_432?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json")
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	data := string(body)
+
+	var jsonObj map[string]interface{}
+	json.Unmarshal([]byte(data), &jsonObj)
+
+	records := jsonObj["records"].([]interface{})
+	//fmt.Println(records)
+
+	for _, r := range records {
+		record := r.(map[string]interface{})
+		//fmt.Println(record["SiteName"])
+		convetedData = append(convetedData, record)
+	}
+}
+
+func findData(site string) string {
+	for _, c := range convetedData {
+		if c["SiteName"].(string) == site {
+			return c["AQI"].(string) + "時間:" + c["ImportDate"].(string)
+		}
+	}
+
+	return "NoData"
 }
